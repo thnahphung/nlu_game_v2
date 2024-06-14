7d1876297be529f8c32e4a894837be5a8fa361dc
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2024-06-14 22:42:34 +0700

    first commit

---
+++ assets/resources/index.js
@@ -0,0 +1,26 @@
+System.register("chunks:///_virtual/en.ts",["cc"],(function(a){var e;return{setters:[function(a){e=a.cclegacy}],execute:function(){e._RF.push({},"80a4feQJTRPIoQoJaUmcS3H","en",void 0);var n=window,t=a("languages",{label_text:{authen_signin:"SIGN IN",authen_signup:"SIGN UP",authen_username:"Username",authen_password:"Password",authen_forgot_password:"Forgot password?",authen_remember_me:"Remember me",authen_email:"Email",authen_confirm_password:"Confirm password",authen_policy:"I agree to all the terms and conditions",setting_title:"SETTING",setting_music:"Music ",setting_sound:"Sound",setting_notify:"Notification",setting_language:"Language",setting_language_current:"English",setting_language_vietnamese:"Tiếng Việt",setting_language_english:"English",pick_character_title:"Pick character",pick_character_select:"PICK",character_title:"Character",character_name_mechanical:"Mechanical Engineer",character_name_veterinarian:"Veterinarian",character_name_agricultural:"Agricultural engineer",character_name_animal_husbandry:"Animal husbandry engineer",loading:"Loading...",btn_setting:"Setting",btn_leader_board:"Leader board",btn_mail:"Mail",btn_event:"Event",btn_quest:"Quest",animal_type:"Type:",animal_age:"Age:",animal_health:"Health:",animal_pregnant:"Pregnant:",animal_age_day:"day",animal_health_good:"Good",animal_health_disease:"Disease",animal_pregnant_yes:"Yes",animal_pregnant_no:"No",breeding_popup_title:"BREEDING",cage_information_popup_title_cow_barn:"COW BARN",cage_information_popup_title_chicken_coop:"CHICKEN COOP",cage_information_popup_capacity:"Capacity:",cage_information_popup_level:"Level:"}});n.languages||(n.languages={}),n.languages.en=t,e._RF.pop()}}}));
+
+System.register("chunks:///_virtual/resources",["./en.ts","./vi.ts","./zh.ts"],(function(){return{setters:[null,null,null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/vi.ts",["cc"],(function(n){var a;return{setters:[function(n){a=n.cclegacy}],execute:function(){a._RF.push({},"eb8a60pxcZP/LG4GoupSwp1","vi",void 0);var e=window,t=n("languages",{label_text:{authen_signin:"ĐĂNG NHẬP",authen_signup:"ĐĂNG KÝ",authen_username:"Tài khoản",authen_password:"Mật khẩu",authen_forgot_password:"Quên mật khẩu?",authen_remember_me:"Tự động đăng nhập lại",authen_email:"Email",authen_confirm_password:"Nhập lại mật khẩu",authen_policy:"Bạn đồng ý với chính sách của chúng tôi!",setting_title:"CÀI ĐẶT",setting_music:"Nhạc nền",setting_sound:"Âm thanh",setting_notify:"Thông báo",setting_language:"Ngôn ngữ",setting_language_current:"Tiếng Việt",setting_language_vietnamese:"Tiếng Việt",setting_language_english:"English",pick_character_title:"Chọn nhân vật",pick_character_select:"CHỌN",character_title:"Nhân vật",character_name_mechanical:"Kỹ sư cơ khí",character_name_veterinarian:"Bác sĩ thú y",character_name_agricultural:"Kỹ sư nông nghiệp",character_name_animal_husbandry:"Kỹ sư chăn nuôi",loading:"Đang tải...",btn_setting:"Cài đặt",btn_leader_board:"Xếp hạng",btn_mail:"Hộp thư",btn_event:"Sự kiện",btn_quest:"Nhiệm vụ",animal_type:"Giống:",animal_age:"Tuổi:",animal_health:"Sức khỏe:",animal_pregnant:"Mang thai:",animal_age_day:"ngày",animal_health_good:"Tốt",animal_health_disease:"Bệnh",animal_pregnant_yes:"Có",animal_pregnant_no:"Không",breeding_popup_title:"LAI GIỐNG",cage_information_popup_title_cow_barn:"CHUỒNG BÒ",cage_information_popup_title_chicken_coop:"CHUỒNG GÀ",cage_information_popup_capacity:"Sức chứa:",cage_information_popup_level:"Cấp độ:"}});e.languages||(e.languages={}),e.languages.vi=t,a._RF.pop()}}}));
+
+System.register("chunks:///_virtual/zh.ts",["cc"],(function(e){var a;return{setters:[function(e){a=e.cclegacy}],execute:function(){a._RF.push({},"d516aeZiaVBnqODlIFzyGl3","zh",void 0);var n=window,t=e("languages",{label_text:{hello:"xin chào!",bye:"tạm biệt!"}});n.languages||(n.languages={}),n.languages.zh=t,a._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/resources', 'chunks:///_virtual/resources'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
